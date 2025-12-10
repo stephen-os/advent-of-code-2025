e99c269d6e0d569f2a622a0f3208ef6f1c8b9956
stephen-os <ImStephen.1sw@gmail.com>
2025-12-09 21:49:47 -0500

    Added solution to day 9 part 1.

---
+++ day09/solution.py
@@ -0,0 +1,35 @@
+﻿# Advent of Code 2025 - Day 9
+
+from collections import deque
+
+lines = []
+with open('input.txt') as file: 
+    lines = file.read().strip().split('\n')
+
+tile_positions = []
+for line in lines:
+    x, y = [int(c) for c in line.split(',')]
+    tile_positions.append((x, y))
+
+def tile_area(position1, position2):
+    w = abs(position1[0] - position2[0] + 1)
+    h = abs(position1[1] - position2[1] + 1)
+    return w * h
+
+# Part 1
+def part1():
+    n = len(tile_positions)
+    max_area = 0 
+    for i in range(n):
+        for j in range(i, n):
+            a = tile_area(tile_positions[i], tile_positions[j])
+            max_area = max(max_area, a)
+    print(max_area)
+
+# Part 2
+def part2():
+    pass
+    
+if __name__ == '__main__':
+    part1()
+    part2()
